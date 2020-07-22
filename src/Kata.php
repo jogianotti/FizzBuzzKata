66475d993454b1246ee77ed86dc9669e34beb031
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2020-07-22 12:58:15 -0300

    Se resuelve detección de múltiplos de ambos

--- src/Kata.php
+++ src/Kata.php
@@ -5,6 +5,10 @@ class Kata
 {
     public function fizzbuzz(int $int)
     {
+        if(0 === $int % 3 && 0 === $int % 5) {
+            return 'FizzBuzz';
+        }
+
         if (0 === $int % 3) {
             return 'Fizz';
         }
