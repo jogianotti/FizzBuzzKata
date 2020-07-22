812ad55fe6026d3da6cc5454c99ec6284a61fa72
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2020-07-22 12:57:41 -0300

    Se prueba múltiplos de ambos

--- tests/KataFizzBuzzTest.php
+++ tests/KataFizzBuzzTest.php
@@ -18,7 +18,7 @@ class KataFizzBuzzTest extends TestCase
         $kata = new Kata();
 
         $this->assertEquals('Fizz', $kata->fizzbuzz(3));
-        $this->assertEquals('Fizz', $kata->fizzbuzz(45));
+        $this->assertEquals('Fizz', $kata->fizzbuzz(48));
         $this->assertEquals('Fizz', $kata->fizzbuzz(99));
     }
 
@@ -29,5 +29,14 @@ class KataFizzBuzzTest extends TestCase
         $this->assertEquals('Buzz', $kata->fizzbuzz(5));
         $this->assertEquals('Buzz', $kata->fizzbuzz(25));
         $this->assertEquals('Buzz', $kata->fizzbuzz(80));
+    }
+
+    public function testCanReplaceMúltiplesOfThreeAndFiveWithFizzBuzz()
+    {
+        $kata = new Kata();
+
+        $this->assertEquals('FizzBuzz', $kata->fizzbuzz(15));
+        $this->assertEquals('FizzBuzz', $kata->fizzbuzz(45));
+        $this->assertEquals('FizzBuzz', $kata->fizzbuzz(75));
     }
 }
